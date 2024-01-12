2940a6b71b1df075b69232f5baeb2d7ee18d0ad1
Broxelen <vasiachellenuk9@gmail.com>
2024-01-12 14:19:43 +0100

    Two

---
+++ Untitled-1.py
@@ -0,0 +1,13 @@
+Name = "Vasyl" 
+
+Age = "13"
+
+City = "Ternovo" 
+
+Subject = "PE"
+
+Height = "+žé"
+
+Pet = "Dog"
+
+print (f"You name {Name}, you are {Age} old, you favorite subject {Subject}, you are from {City}, you height {Height}, you favorite oet {Pet}")

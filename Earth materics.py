2c4cf9c573476ed28c8f95684eca764718317d79
Broxelen <vasiachellenuk9@gmail.com>
2024-02-09 18:49:03 +0100

    Materics

---
+++ Earth materics.py
@@ -0,0 +1,10 @@
+my_earth = ["North America", "South America", "Antarctica"]
+my_earthtwo = ["Eurasia", "Africa", "Australia", "Antarctica"]
+
+for i in my_earthtwo:
+    my_earth.append(i)
+
+my_earth.sort()
+
+print(my_earth)
+    
